b32e6cce4a9d35b54588a56f5e27174bfd26d5d2
samer Samara <samer.w.samara@gmail.com>
2024-09-07 07:46:46 +0300

    make the log in and sign up pages

--- task2/src/App.js
+++ task2/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from "./pages/LoginPage"
+import Regstraion from './pages/Regstraion';
 function App() {
   return (
     <>
-    <LoginPage />
+   <BrowserRouter>
+      <Routes>
+        <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/Regstraion" element={<Regstraion />} />
+      </Routes>
+   </BrowserRouter>
     </>
   );
 }
---
+++ task2/src/pages/Regstraion.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "../style/LoginPage.css"
+function Regstraion() {
+  return (
+    <div className='container-fluid 100-w vh-100'>
+    <div className='row'>
+        <div className='col-12 d-flex justify-content-center pt-5 mt-5'>
+            <div className='login-wrapper w-25 p-3 rounded '>
+                <h1 className='text-center'>Regstraion</h1>
+                <form>
+                <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">User Name</label>
+                        <input type="text" className="form-control rounded" id="exampleInputPassword1"/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control rounded " id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control rounded" id="exampleInputPassword1"/>
+                    </div>
+                
+                    <button type="submit" className="btn ps-5 pe-5 rounded">Sign up</button>
+                    </form>
+                    
+            </div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default Regstraion
